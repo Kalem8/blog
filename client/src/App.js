b9849a454bb3d0965e68e4a5945f83206910a263
Yildiz Yunus <yunusyildiz44800@gmail.com>
2024-01-25 14:53:50 +0100

    Ajout du CSS et du contenu

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <a href="" className="logo"> MyBlog </a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
       </header>
-    </div>
-  );
+
+      <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2014/10/money-cash-money-cash.jpg?w=1390&crop=1" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Lorem, ipsum.</h2>
+          <p className='info'>
+            <a className='author'> Yunus Yildiz </a>
+            <time>2023 01-06 16:45</time>
+          </p>
+          <p className='summary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt dolores eaque dignissimos, natus labore,
+            quas alias obcaecati vero non unde sit aut animi illo repudiandae!</p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2014/10/money-cash-money-cash.jpg?w=1390&crop=1" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Lorem, ipsum.</h2>
+          <p className='info'>
+            <a className='author'> Yunus Yildiz </a>
+            <time>2023 01-06 16:45</time>
+          </p>
+          <p className='summary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt dolores eaque dignissimos, natus labore,
+            quas alias obcaecati vero non unde sit aut animi illo repudiandae!</p>
+        </div>
+      </div>
+
+      <div className="post">
+        <div className="image">
+          <img src="https://techcrunch.com/wp-content/uploads/2014/10/money-cash-money-cash.jpg?w=1390&crop=1" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Lorem, ipsum.</h2>
+          <p className='info'>
+            <a className='author'> Yunus Yildiz </a>
+            <time>2023 01-06 16:45</time>
+          </p>
+          <p className='summary'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt dolores eaque dignissimos, natus labore,
+            quas alias obcaecati vero non unde sit aut animi illo repudiandae!</p>
+        </div>
+      </div>
+
+    </main>
+  )
 }
 
 export default App;
